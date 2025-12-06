d5f40a240cbecef3ff32c02e4eb828733b1dca04
navin2709 <navinnit2006@gmail.com>
2025-12-06 21:32:26 +0530

    profile done excepte settings and edit

--- app/(site)/client/profile/page.jsx
+++ app/(site)/client/profile/page.jsx
@@ -1,16 +1,32 @@
 'use client';
+import React,{useState,useEffect} from 'react';
 import ProfileHeader from '@/components/client/profile/ProfileHeader';
-import ProfileStats from '@/components/client/profile/ProfileStats';
 import UserQueries from '@/components/client/profile/UserQueries';
 import ProfileActions from '@/components/client/profile/ProfileActions';
-import { mockQueries } from '@/lib/mockData';
+import axios from 'axios';
+import { useSession } from 'next-auth/react';
 
 export default function ProfilePage() {
+  const [queries, setQueries] = useState([]);
+  const { data: session } = useSession(); 
+
+  useEffect(() => {
+    const fetchQueries = async () => {
+      try {
+        const response = await axios.get('/api/client/queries');
+        const userresponses = response.data.filter(query => query.clientId === session?.user?.id);
+        setQueries(userresponses);
+      } catch (error) {
+        console.error('Error fetching queries:', error);
+      }   
+    }
+    fetchQueries();
+  }, []);
+
   return (
     <div className="animate-fadeIn max-w-2xl">
       <ProfileHeader />
-      <ProfileStats />
-      <UserQueries queries={mockQueries} />
+      <UserQueries queries={queries} />
       <ProfileActions />
     </div>
   );
